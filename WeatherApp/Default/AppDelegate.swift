6eec093a6ec3fe6021ed7baac2bd8083bc236bed
Dmitriy Danilin <danilindn@icloud.com>
2024-03-05 11:54:47 +0300

    Подготовка к разработке
    - Удаление лишних файлов и создание нужных групп
    - Добавление гитигнор
    - Подготовка к созданию UI через код

---
+++ WeatherApp/Default/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  WeaterApp
+//
+//  Created by Дмитрий Данилин on 05.03.2024.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	
+	var window: UIWindow?
+	
+	func application(
+		_ application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+	) -> Bool {
+		
+		createAndShowStartVC()
+		
+		return true
+	}
+}
+
+// MARK: - Initial application settings
+
+private extension AppDelegate {
+	/// Создание и отображение стартового ViewController
+	func createAndShowStartVC() {
+		let mainVC = ViewController()
+		let navigationController = UINavigationController(rootViewController: mainVC)
+		
+//		PresentationAssembly().taskList.config(
+//			view: mainVC,
+//			navigationController: navigationController
+//		)
+
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.rootViewController = navigationController
+		window?.makeKeyAndVisible()
+	}
+}
+
